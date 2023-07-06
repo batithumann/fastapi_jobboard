b430a362fc21e5f3cad735d40557a56ae6b429ff
Walter Thumann <wthumann@gmail.com>
2023-07-06 17:15:21 -0400

    authentication with jwt

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -4,14 +4,16 @@ from typing import List
 
 from schemas.jobs import JobCreate, ShowJob
 from db.models.jobs import Job
+from db.models.users import User
 from db.session import get_db
 from db.repository.jobs import (create_new_job, get_job_by_id, get_active_jobs, update_job_by_id, delete_job_by_id)
+from apis.version1.route_login import get_current_user_from_token
 
 router = APIRouter()
 
 @router.post("/create_job", response_model=ShowJob)
-def create_job(job:JobCreate, db:Session=Depends(get_db)):
-    owner_id = 1
+def create_job(job:JobCreate, db:Session=Depends(get_db), current_user: User=Depends(get_current_user_from_token)):
+    owner_id = current_user.id
     job = create_new_job(job=job, db=db, owner_id=owner_id)
     return job
 
@@ -41,10 +43,12 @@ def update_job(id:int, job:JobCreate, db:Session=Depends(get_db)):
 
 
 @router.delete("/delete/{id}")
-def delete_job(id:int, db:Session=Depends(get_db)):
-    owner_id = 1
-    message = delete_job_by_id(id, db, owner_id)
-    if not message:
+def delete_job(id:int, db:Session=Depends(get_db), current_user: User=Depends(get_current_user_from_token)):
+    job = get_job_by_id(id, db)
+    if not job:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Job with id {id} does not exist")
-    return {"detail":"Job successfully deleted"}
+    if job.owner_id == current_user.id or current_user.is_superuser:
+        delete_job_by_id(id, db, owner_id=current_user.id)
+        return {"detail": "Job successfully deleted"}
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not permitted")
